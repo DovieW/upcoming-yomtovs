2237e4569f52dca99156b93a19201d2319d5d93c
Yitzy Holtzberg <yitzyh18@gmail.com>
2024-04-09 11:48:43 -0400

    Day of week

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
-import React, {useEffect, useState} from "react";
-import { Appbar, Card, Paragraph, Provider as PaperProvider, Text, Title } from 'react-native-paper';
-import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import React, {useEffect, useState} from 'react';
+import {
+  Appbar,
+  Card,
+  Paragraph,
+  Provider as PaperProvider,
+  Text,
+  Title,
+} from 'react-native-paper';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {Linking, FlatList} from "react-native";
+import {Linking, FlatList} from 'react-native';
 
 interface YomTov {
   title: string;
@@ -14,36 +21,38 @@ interface YomTov {
   location: string;
 }
 
-function YomTovCard({ yomTov }: {yomTov: YomTov}) {
+function YomTovCard({yomTov}: {yomTov: YomTov}) {
   return (
-    <Card style={{ margin: 10 }}>
-      <Card.Content>
-        <Title>{yomTov.title}</Title>
-        <Text>{yomTov.date}</Text>
-        <Text>{generateDaysUntil(yomTov.date)}</Text>
-        <Text>{yomTov.hebrew}</Text>
-        <Text>{yomTov.category}</Text>
-        <Paragraph>{yomTov.memo}</Paragraph>
-        <Text
-          onPress={() => Linking.openURL(yomTov.link)}
-          style={{color: 'teal'}}>
-          Learn more
-        </Text>
-        <Text>{yomTov.location}</Text>
-      </Card.Content>
-    </Card>
+      <Card style={{margin: 10}}>
+        <Card.Content>
+          <Title>{yomTov.title}</Title>
+          <Text>
+            {yomTov.date} - {generateDayOfWeek(yomTov.date)}
+          </Text>
+          <Text>{generateDaysUntil(yomTov.date)}</Text>
+          <Text>{yomTov.hebrew}</Text>
+          <Text>{yomTov.category}</Text>
+          <Paragraph>{yomTov.memo}</Paragraph>
+          <Text
+              onPress={() => Linking.openURL(yomTov.link)}
+              style={{color: 'teal'}}>
+            Learn more
+          </Text>
+          <Text>{yomTov.location}</Text>
+        </Card.Content>
+      </Card>
   );
 }
 
-function YomTovList({ yomTovs }: {yomTovs: YomTov[]}) {
+function YomTovList({yomTovs}: {yomTovs: YomTov[]}) {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <FlatList
-        data={yomTovs}
-        renderItem={({ item }) => <YomTovCard yomTov={item} />}
-        keyExtractor={(item, index) => index.toString()}
-      />
-    </SafeAreaView>
+      <SafeAreaView style={{flex: 1}}>
+        <FlatList
+            data={yomTovs}
+            renderItem={({item}) => <YomTovCard yomTov={item} />}
+            keyExtractor={(item, index) => index.toString()}
+        />
+      </SafeAreaView>
   );
 }
 
@@ -55,19 +64,35 @@ function generateDaysUntil(date: string): string {
   return `Days until: ${days}`;
 }
 
+function generateDayOfWeek(date: string) {
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const dateObj = new Date(date).getDay();
+  return days[dateObj];
+}
+
 function App() {
   const [yomTovs, setYomTovs] = useState<YomTov[]>([]);
 
   useEffect(() => {
     const date = new Date();
     const currentYear = date.getFullYear();
-    const currentMonth = (date.getMonth() + 1).toString().padStart(2, "0");
-    const currentDay = date.getDate().toString().padStart(2, "0");
+    const currentMonth = (date.getMonth() + 1).toString().padStart(2, '0');
+    const currentDay = date.getDate().toString().padStart(2, '0');
     const startDate = `${currentYear}-${currentMonth}-${currentDay}`;
     const endDate = `${currentYear + 1}-${currentMonth}-01`;
     (async () => {
       try {
-        const response = await fetch(`https://www.hebcal.com/hebcal?start=${startDate}&end=${endDate}&v=1&cfg=json&maj=on&min=on&mod=off&nx=on&month=x&ss=on&mf=on&c=off&s=off&D=off&d=off&o=off&F=off&myomi=off&leyning=off`);
+        const response = await fetch(
+            `https://www.hebcal.com/hebcal?start=${startDate}&end=${endDate}&v=1&cfg=json&maj=on&min=on&mod=off&nx=on&month=x&ss=on&mf=on&c=off&s=off&D=off&d=off&o=off&F=off&myomi=off&leyning=off`,
+        );
         const data = await response.json();
         setYomTovs(data.items);
         // await AsyncStorage.setItem('yomTovs', JSON.stringify(yomTovs));
@@ -78,14 +103,14 @@ function App() {
   }, []);
 
   return (
-    <SafeAreaProvider>
-      <PaperProvider>
-        <Appbar.Header>
-          <Appbar.Content title="Yom Tov Reminders" />
-        </Appbar.Header>
-        <YomTovList yomTovs={yomTovs} />
-      </PaperProvider>
-    </SafeAreaProvider>
+      <SafeAreaProvider>
+        <PaperProvider>
+          <Appbar.Header>
+            <Appbar.Content title="Yom Tov Reminders" />
+          </Appbar.Header>
+          <YomTovList yomTovs={yomTovs} />
+        </PaperProvider>
+      </SafeAreaProvider>
   );
 }
 
