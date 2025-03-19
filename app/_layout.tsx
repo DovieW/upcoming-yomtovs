f64c01907488fb5f63f70671b1ed493b527c6377
Dovie Weinstock <DovieWeinstock@gmail.com>
2025-03-19 19:05:54 -0400

    remove white to bar but still white top top

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,8 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
-import { Stack } from 'expo-router';
+import { SplashScreen, Stack } from 'expo-router';
 import { useFonts } from 'expo-font';
-import { SplashScreen } from 'expo-router';
 import { lightTheme, darkTheme } from '../constants/Theme';
 
 export default function RootLayout() {
@@ -30,7 +29,10 @@ export default function RootLayout() {
 
   return (
     <PaperProvider theme={theme}>
-      <Stack>
+      <Stack screenOptions={{ headerShown: false }}>
+        {/* Hide the header for the "(tabs)" group */}
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        {/* Hide the header for the root "index" screen */}
         <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
     </PaperProvider>
